2c97ec198a325bf7d6287daa630178214ab23215
CaffeinatedAndroid <251544638+CaffeinatedAndroid@users.noreply.github.com>
2026-03-25 00:38:33 +1000

    Main Menu Fucntions, Basic Data Saving, Load Scene, GameManager
    Main menu:
    - New game loads scene
    -load game is partially implemented with basic data storage and retrieval
    -exit button is configured to close the game when built
    
    Game Manger:
    - expanded functions, contains save and load, very modular

---
+++ Assets/Scripts/GameManager/MainMenu.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private static MainMenu instance;
+    public string mainLevel;
+    public string loadLevel;
+
+    public Button newGameButton;
+    public Button loadButton;
+    public Button quitButton;
+
+    void awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        newGameButton.onClick.AddListener(LoadLevel);
+      //  loadButton.onClick.AddListener(loadGame);
+        quitButton.onClick.AddListener(QuitGame);
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        return;
+    }
+    
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(mainLevel);
+        
+    }
+
+    //void loadGame()
+    //{
+    //    SceneManager.LoadScene(mainLevel);
+
+    //    Scene scene = SceneManager.GetSceneByName(mainLevel); //Confirm that scene is loaded before loading game data
+    //    Debug.Log($"{mainLevel} is loaded.");
+        
+    //    GameData data = SaveLoad.LoadData();
+    //    Debug.Log($"Game Data is loaded.");
+    //    //this.gameObject.SetActive(false);
+
+    //}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/GameManager/SaveLoad.cs
@@ -0,0 +1,40 @@
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public static class SaveLoad
+{
+    public static void SaveData(GameManager data)//GameManager is the GameManager.cs script, GameManager data script
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/Gamesave";
+
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData sedata = new GameData(data);
+
+        formatter.Serialize(stream, sedata);
+        stream.Close();
+    }
+
+    public static GameData LoadData()
+    {
+        string path = Application.persistentDataPath + "/Gamesave";
+
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = formatter.Deserialize(stream) as GameData;
+
+            stream.Close();
+
+            return data;
+        } else
+        {
+            Debug.LogError("Error: Save file not found in " + path);
+            return null;
+        }
+    }
+}
