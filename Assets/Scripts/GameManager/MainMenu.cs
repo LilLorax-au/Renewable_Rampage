dd6013a6d348eb306fbc1a9dc2cd2f3925a24eca
CaffeinatedAndroid <251544638+CaffeinatedAndroid@users.noreply.github.com>
2026-03-25 23:57:44 +1000

    Added version tracker to menu, clearer title

--- Assets/Scripts/GameManager/MainMenu.cs
+++ Assets/Scripts/GameManager/MainMenu.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor.Build.Reporting;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     private static MainMenu instance;
     public string mainLevel;
     public string loadLevel;
-
+    public TextMeshProUGUI version;
     public Button newGameButton;
     public Button loadButton;
     public Button quitButton;
@@ -24,17 +26,19 @@ public class MainMenu : MonoBehaviour
     //        Destroy(gameObject);
     //    }
     //}
-    
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+        GetVersion();
         newGameButton.onClick.AddListener(LoadLevel);
-      //  loadButton.onClick.AddListener(loadGame);
+        //  loadButton.onClick.AddListener(loadGame);
         quitButton.onClick.AddListener(QuitGame);
 
 
-        
+
     }
 
     // Update is called once per frame
@@ -42,11 +46,11 @@ public class MainMenu : MonoBehaviour
     {
         return;
     }
-    
+
     public void LoadLevel()
     {
         SceneManager.LoadScene(mainLevel);
-        
+
     }
 
     //void loadGame()
@@ -55,7 +59,7 @@ public class MainMenu : MonoBehaviour
 
     //    Scene scene = SceneManager.GetSceneByName(mainLevel); //Confirm that scene is loaded before loading game data
     //    Debug.Log($"{mainLevel} is loaded.");
-        
+
     //    GameData data = SaveLoad.LoadData();
     //    Debug.Log($"Game Data is loaded.");
     //    //this.gameObject.SetActive(false);
@@ -65,5 +69,10 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    void GetVersion()
+    {
+        version.text =  Application.version;
     }
 }
