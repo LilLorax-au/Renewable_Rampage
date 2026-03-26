89cd054b0385270124a5d5de255c4764f8f7a6f7
CaffeinatedAndroid <251544638+CaffeinatedAndroid@users.noreply.github.com>
2026-03-26 13:55:52 +1000

    Parallax Background, Basis for procedural level themes

---
+++ Assets/Scripts/GameManager/ScrollBackground.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = -5f;
+    public Vector2 startPos;
+    public int distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, distance);
+        transform.position = startPos + Vector2.left * newPos;
+    }
+}
+
+
--- Assets/Scripts/TitleAnim.cs
+++ Assets/Scripts/TitleAnim.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TitleAnim : MonoBehaviour
 {
+    public GameObject parent;
     public float frequency = 1.0f;
     public float magnitude = 1.0f;
     public float elapsedTime = 0f;
     public float speed = 0.5f;
+    public float offsetDistance;
 
 
     private Vector3 pos;
@@ -18,8 +22,12 @@ public class TitleAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        transform.position = pos + (transform.up * Mathf.Sin(elapsedTime * frequency) * magnitude);
-        //transform.position += Vector3.left * speed * Time.deltaTime;
+        if (parent != null)
+        {
+            Vector3 offset = new Vector3(0, offsetDistance, 0);
+            elapsedTime += Time.deltaTime;
+            transform.position = pos + (transform.up * Mathf.Sin(elapsedTime * frequency) * magnitude) + parent.transform.position + offset;
+            //transform.position += Vector3.left * speed * Time.deltaTime;
+        }
     }
 }
